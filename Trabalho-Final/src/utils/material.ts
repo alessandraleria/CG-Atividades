afbf49cac1cfdc8daccc6734a12e8831737c55bc
Alessandra Leria <57183427+alessandraleria@users.noreply.github.com>
2024-02-14 19:28:55 -0300

    Delete Trabalho-Final directory

--- Trabalho-Final/src/utils/material.ts
+++
@@ -1,7 +0,0 @@
-import { Color } from './Color';
-import { HitRecord } from './hittable/hittable';
-import { RayT } from './ray';
-
-export abstract class Material {
-  abstract scatter(rIn: RayT, rec: HitRecord): { isScatter: boolean; scattered: RayT; attenuation: Color };
-}
--- Trabalho-Final/src/utils/point/point_3.ts
+++
@@ -1,7 +0,0 @@
-import { Vec3 } from "../vec/vec_3";
-
-export class Point3 extends Vec3 {
-  constructor(x: number, y: number, z: number) {
-    super(x, y, z);
-  }
-}
